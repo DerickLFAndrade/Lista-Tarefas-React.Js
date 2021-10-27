e10dbbec7c0ed794af7fdc3daf7ca72ea9ca228b
DerickLFAndrade <derickspy15@gmail.com>
2021-10-26 21:29:06 -0300

    Lista de tarefas em React.js

---
+++ src/App.styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+background-color: #17181F;
+color: #ffff ;
+min-height: 100vh;
+`;
+
+export const Area = styled.div`
+ margin: auto;
+ max-width 980px;
+ padding: 10px;
+`;
+
+export const Header = styled.h1`
+margin: 0;
+padding: 0;
+color: #ffff;
+text-align: center;
+border-bottom: 1px solid #444;
+padding-bottom: 20px
+
+`;
+
+export const Lista = styled.li `
+margin-top: 10px;
+background-color: rgba(0, 0, 0, 0.4);
+padding: 10px;
+border-radius: 5px;
+color: #ffff;
+font-size: 20px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/item';
+const App = () => {
+  
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Atividade Exemplo', done: false},
+   
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  <C.Container>
+    <C.Area>
+      <C.Header>Lista de Tarefas</C.Header>
+
+      {/*Area de adicionar*/}
+      
+      {list.map((item, index) => (
+        <C.Lista>{item.name}</C.Lista>
+      ))}
+
+    </C.Area>
+  </C.Container>
+    </>
+  )
 }
 
 export default App;
